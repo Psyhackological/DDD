7170e1ab3cd1c3bb057cf714498e0ddcfc3bec6a
Psyhackological <konradkon@duck.com>
2022-05-13 12:47:50 +0200

    Fixed a bug, deleted 1 from range, changed print to f-string
    Fixed a bug with -c, --choices option (without a flag it overrided everything with its default option).
    Deleted from range step option.
    Changed print fo f-string, because f-strings are cool.

--- DDD.py
+++ DDD.py
@@ -16,8 +16,8 @@ parser.add_argument(nargs='?', default=1, type=episode_int,
                     help="starts downloading from this episode number. (int) (default=1)", dest="start")
 parser.add_argument(nargs='?', default=DDJ.recent_episode_nr, type=episode_int,
                     help=f"ends downloading to number. (int) (default={DDJ.recent_episode_nr})", dest="end")
-parser.add_argument('-c', "--choices", nargs='+', default=range(1, DDJ.recent_episode_nr + 1, 1), type=episode_int,
-                    help=f"gets any int arguments and then downloads them. (list_with_ints) (default=1..{DDJ.recent_episode_nr})")
+parser.add_argument('-c', "--choices", nargs='+', type=episode_int,
+                    help=f"gets any int arguments and then downloads them. (list_with_ints) (default=None)")
 args = parser.parse_args()
 
 
@@ -25,16 +25,15 @@ def download_podcast():
     with open("DD.json") as json_file:
         json_py_dict = json.load(json_file)
 
-    from_to = args.choices if args.choices else range(args.start, args.end + 1, 1)
+    from_to = args.choices if args.choices else range(args.start, args.end + 1)
     print("\nDownloading started...")
     for i in from_to:
         link = json_py_dict[str(i)]["link"]
         title = json_py_dict[str(i)]["title"]
 
-        print(title + ': ' + link)
+        print(f"{title}: {link}")
 
         episode = requests.get(link)
-
         with open(title + ".mp3", 'wb') as mp3_file:
             mp3_file.write(episode.content)
 
